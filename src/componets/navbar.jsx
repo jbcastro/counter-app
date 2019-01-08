96d1442d3f7a158d9a16c29812fe2fdec754f136
jbcastro <josephbeckcastro@gmail.com>
2019-01-08 14:52:07 -0800

    11 commit, 208minutes

--- src/componets/navbar.jsx
+++ src/componets/navbar.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
-
-class NavBar extends Component {
-  render() {
-    return (
-      <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          NavBar
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+//Stateless Functional Component
+const NavBar = props => {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        NavBar
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
