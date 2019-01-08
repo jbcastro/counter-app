95fa1a2dbde0bc6c918aab126e55bb95e70486e0
jbcastro <josephbeckcastro@gmail.com>
2019-01-08 14:54:34 -0800

    12 commit, 210minutes

--- src/componets/navbar.jsx
+++ src/componets/navbar.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 //Stateless Functional Component
-const NavBar = props => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav class="navbar navbar-light bg-light">
       <a class="navbar-brand" href="#">
         NavBar
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
